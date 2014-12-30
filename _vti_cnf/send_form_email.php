da8b0a16327168a4055084e03868feec80df93b6
timcampix <homare.takase@gmail.com>
2014-12-30 13:30:39 -0500

    Contact Form
    added php

---
+++ _vti_cnf/send_form_email.php
@@ -0,0 +1,14 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|09 Dec 2014 18:03:08 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|HOMA\\homar_000
+vti_modifiedby:SR|HOMA\\homar_000
+vti_timecreated:TR|09 Dec 2014 18:03:08 -0000
+vti_cacheddtm:TX|09 Dec 2014 18:03:08 -0000
+vti_filesize:IR|2876
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
+vti_backlinkinfo:VX|
