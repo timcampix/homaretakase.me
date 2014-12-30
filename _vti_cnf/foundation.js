1e5473af1d1aa89f85b3eaad4c8827e40494768b
timcampix <homare.takase@gmail.com>
2014-12-30 12:52:03 -0500

    Updated Website
    portfolio site

---
+++ _vti_cnf/foundation.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|15 Feb 2014 21:43:25 -0000
+vti_author:SR|HOMA\\homar_000
+vti_modifiedby:SR|HOMA\\homar_000
+vti_nexttolasttimemodified:TW|15 Feb 2014 21:43:25 -0000
+vti_timecreated:TR|30 Dec 2014 17:03:51 -0000
+vti_cacheddtm:TX|30 Dec 2014 17:03:51 -0000
+vti_filesize:IR|16600
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Web\\ Design\\ Class/Final\\ Project/timcampix.github.io/index.html
--- _vti_cnf/modernizr.js
+++ _vti_cnf/modernizr.js
@@ -1,9 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|01 Oct 2014 09:39:24 -0000
+vti_timelastmodified:TW|15 Feb 2014 21:43:25 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|HOMA\\homar_000
 vti_modifiedby:SR|HOMA\\homar_000
 vti_timecreated:TR|01 Oct 2014 09:39:24 -0000
-vti_cacheddtm:TX|01 Oct 2014 09:39:24 -0000
-vti_filesize:IR|11262
 vti_backlinkinfo:VX|Web\\ Design\\ Class/Final\\ Project/timcampix.github.io/index.html
+vti_nexttolasttimemodified:TW|15 Feb 2014 21:43:25 -0000
+vti_cacheddtm:TX|30 Dec 2014 16:35:50 -0000
+vti_filesize:IR|11252
